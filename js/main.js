c6cdd006178b0bcccccd13de63dd2fe5cec19d8c
zawzawzaw <zawzawzaw@gmail.com>
2014-09-22 18:16:06 +0800

    updated

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
 $(document).ready(function(){
 	console.log('hi')
+
+	$("#nav-mobile").html($("#nav-main").html());
+    $("#nav-trigger span").click(function(){
+        if ($("#nav-mobile ul").hasClass("expanded")) {
+            $("#nav-mobile ul.expanded").removeClass("expanded").slideUp(250);
+            $(this).removeClass("open");
+        } else {
+            $("#nav-mobile ul").addClass("expanded").slideDown(250);
+            $(this).addClass("open");
+        }
+    });
+
+    
 });

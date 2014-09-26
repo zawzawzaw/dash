848ec65f7f43bcf4a03610f8a56da62d95155783
zawzawzaw <zawzawzaw@gmail.com>
2014-09-26 15:05:13 +0800

    updated

--- js/main.js
+++ js/main.js
@@ -2,11 +2,13 @@ $(document).ready(function(){
 	console.log('hi');
 
     $('.carousel').carousel({
-      interval: 20000
+        interval: 9000,
+        pause: "false"
     });
 
 	$("#nav-mobile").html($("#nav-main").html());
     $("#nav-trigger span").click(function(){
+        console.log('hi')
         if ($("#nav-mobile ul").hasClass("expanded")) {
             $("#nav-mobile ul.expanded").removeClass("expanded").slideUp(250);
             $(this).removeClass("open");
@@ -14,6 +16,24 @@ $(document).ready(function(){
             $("#nav-mobile ul").addClass("expanded").slideDown(250);
             $(this).addClass("open");
         }
+    });
+
+    // $('.login').colorbox({href:"login.shtml"});
+    $('a.login').fancybox({
+        padding: 50,
+        width: 300,
+        height: 300,
+        autoDimensions: false,
+        closeBtn : false 
+    });
+
+    $(window).scroll(function() {
+    if ($(this).scrollTop() > 1){  
+        $('#header-wrapper').addClass("sticky");
+      }
+      else{
+        $('#header-wrapper').removeClass("sticky");
+      }
     });
 
     
